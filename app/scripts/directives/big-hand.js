6c3ec42455fd556bc43013a0f8e5b3ef52f67991
James Pants <jamespants@gmail.com>
2015-01-20 16:17:20 +0100

    white design

--- app/scripts/directives/big-hand.js
+++ app/scripts/directives/big-hand.js
@@ -10,7 +10,9 @@ angular.module('variousAssetsApp').directive('bigHand', ['mousePosition', functi
 	
 	var link = function($scope, element, attrs){
 
-		var frame = $('#main-frame');
+		var frameId = attrs.frameId;
+		console.log(frameId);
+		var frame = $('#' + frameId);
 
 		var pos = mousePosition.getPos();
 
@@ -18,19 +20,25 @@ angular.module('variousAssetsApp').directive('bigHand', ['mousePosition', functi
 		var yPos = pos.y;
 
 		//set initial css based on prev position
-		element.css({
-			left: xPos + 10,
-			top: yPos + 10
-		});
+		// element.css({
+		// 	left: xPos + 5,
+		// 	top: yPos + 5
+		// });
+
+
+		element.transition({
+			'x': xPos + 5,
+			'y': yPos + 5
+		},0);
 
 		//now, listen for changes
 		frame.on('mousemove', function(e){
 			xPos = e.pageX;
 			yPos = e.pageY;
-			element.css({
-				left: xPos + 10,
-				top: yPos + 10
-			});
+			element.transition({
+				'x': xPos + 5,
+				'y': yPos + 5
+			},0);
 		})
 		
 	}
--- app/scripts/directives/waypoint.js
+++ app/scripts/directives/waypoint.js
@@ -13,6 +13,7 @@ angular.module('variousAssetsApp').directive('waypointStick', function () {
 		var waypoint = new Waypoint({
 			element: document.getElementById('waypoint'),
 			offset: '40',
+			context: document.getElementById('about-wrapper'),
 			handler: function(direction){
 				if (direction === 'down'){
 					element.addClass('sticky');
